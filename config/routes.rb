9f904951cf4a667d5bbf81051e93573824ddb902
Minhal <mshanjer@gmail.com>
2018-08-16 21:38:16 -0400

    View in reasonable condition

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   	get 'login', to: 'devise/sessions#new'
   end
 
+  devise_scope :user do
+  	get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 
   root to: 'pages#index'
 end
