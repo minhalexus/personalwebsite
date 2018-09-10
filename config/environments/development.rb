5d9eba5da773c53bffcefeef781f375cb4545e77
Minhal Shanjer <mshanjer@gmail.com>
2018-09-09 22:23:37 -0400

    removed config.file_watcher from both

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.exceptions_app = self.routes
 end
